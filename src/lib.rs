e35dbbab66fee734829cb3c8a40bc7a5efaa7cd9
jojonv <novakj@outlook.com>
2015-05-25 14:33:39 +0200

    added Result type

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,8 @@ const FORMAT_MESSAGE_ARGUMENT_ARRAY: u32 = 0x00002000;
 const UNKNOWN_ERROR_TEXT: &'static str = "Unknown error";
 
 
+pub type Win32Result<T> = Result<T, Win32Error>;
+
 #[derive(Debug, Clone)]
 pub struct Win32Error
 {
